3f58f6a46b3a50af642a43c2a24803c2982443ce
Ian Hunter <ian.hunter@sjsu.edu>
2017-03-19 23:00:41 -0700

    first commit

---
+++ tester.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+import scholar
+import sys, getopt, os
+
+
+def searchGoogle(textSearch):
+    print("Starting Google Scholar Search")
+    os.system("scholar.py -c 5 --phrase %s" % textSearch)
+
+def main(argv):
+   textSearch = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+   except getopt.GetoptError:
+      print ("test.py -i <textSearch> -o <outputfile>")
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print ("test.py -i <textSearch> -o <outputfile>")
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         textSearch = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+   print('Text Search is "%s"' % textSearch)
+   print ('Output file is "%s"' % outputfile)
+   
+   searchGoogle(textSearch)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+   
