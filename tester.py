fbfb30f6614909ce9770c589bc7fbba1402c8693
Ian Hunter <ian.hunter@sjsu.edu>
2017-04-22 12:18:54 -0700

    class separation works now for tester.py calling TestTfIDF

--- tester.py
+++ tester.py
@@ -1,11 +1,15 @@
 from bs4 import BeautifulSoup
 import scholar
 import sys, getopt, os
+from pdf2text import convert_pdf_to_txt
+from tfidf import tfidf
+from TestTfIDF import TestTfIDF
+import time
 
 
 def searchGoogle(textSearch):
     print("Starting Google Scholar Search")
-    os.system("scholar.py -c 40 --phrase %s" % textSearch)
+    os.system("scholar.py -c 40 --phrase %s > output.txt" % textSearch)
 
 def main(argv):
    textSearch = ''
@@ -26,10 +30,23 @@ def main(argv):
    print('Text Search is "%s"' % textSearch)
    print ('Output file is "%s"' % outputfile)
    
+       
+   
    searchGoogle(textSearch)
+   #delay 20 seconds, wait for output.txt to be generated
+   time.sleep(5)
+   print('Start tfidf')
+   
+   
+   #create tfidf tester class
+   tfidfTest2 = TestTfIDF()
+   #f = open("output.txt", 'w')
+   keywords = ['government' 'USA']
+   tfidfTest2.calc(keywords)
+   
 
 if __name__ == "__main__":
    main(sys.argv[1:])
 
-   
+
          
